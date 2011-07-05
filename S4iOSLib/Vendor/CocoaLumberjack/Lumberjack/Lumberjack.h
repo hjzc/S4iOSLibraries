7fd35d0d16434435a41be8d6811196470c4c4268
Michael Papp <mikekppp@aim.com>
2011-07-04 23:27:34 -0700

    adding header

---
+++ S4iOSLib/Vendor/CocoaLumberjack/Lumberjack/Lumberjack.h
@@ -0,0 +1,11 @@
+//
+//  Lumberjack.h
+//  Lumberjack
+//
+
+#include <Lumberjack/DDLog.h>
+#include <Lumberjack/DDAbstractDatabaseLogger.h>
+#include <Lumberjack/DDASLLogger.h>
+#include <Lumberjack/DDFileLogger.h>
+#include <Lumberjack/DDTTYLogger.h>
+
