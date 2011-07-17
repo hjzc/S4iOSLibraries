b981bdb2a382932233731f6fefbac89fd00f1779
Michael Papp <mikekppp@aim.com>
2011-07-17 13:00:32 -0700

    adding fork of TouchJSON library

---
+++ S4iOSLib/Vendor/TouchJSON/Support/NewBenchmark/TouchJSONBenchmark.m
@@ -0,0 +1,46 @@
+//
+//  TouchJSONBenchmark.m
+//  TouchJSON
+//
+//  Created by Jonathan Wight on 1/1/2000.
+//  Copyright 2011 toxicsoftware.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "CJSONDataSerializer.h"
+#import "CJSONDeserializer.h"
+
+int main (int argc, const char * argv[])
+{
+NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+
+NSDictionary *theDictionary = [NSDictionary dictionaryWithObjectsAndKeys:
+    @"Hello", @"World",
+    NULL];
+    
+CJSONDataSerializer *theSerializer = [CJSONDataSerializer serializer];
+CJSONDeserializer *theDeserializer = [CJSONDeserializer deserializer];
+
+NSData *theData = [theSerializer serializeObject:theDictionary error:NULL];
+
+CFAbsoluteTime theStart = CFAbsoluteTimeGetCurrent();
+for (int N = 0; N != 2000000; ++N)
+    {
+////    [theDeserizl serializeObject:theDictionary error:NULL];
+    NSAutoreleasePool *thePool = [[NSAutoreleasePool alloc] init];
+    [theDeserializer deserialize:theData error:NULL];
+    [thePool release];
+    }
+
+
+
+CFAbsoluteTime theEnd = CFAbsoluteTimeGetCurrent();
+
+NSLog(@"%g", theEnd - theStart);
+
+
+[pool drain];
+return 0;
+}
